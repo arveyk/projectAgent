481e790990b6a2a07b61b4f15aefa49cfd3bc2b6
arveyk <harveykisiangani@gmail.com>
2025-06-19 19:22:59 +0300

    test agent

---
+++ testagent.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""
+Automate Worlflow testing
+"""
+import requests
+
+import logging
+import os
+# Import WebClient from Python SDK (github.com/slackapi/python-slack-sdk)
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
+
+# WebClient instantiates a client that can call API methods
+# When using Bolt, you can use either `app.client` or the `client` passed to listeners.
+client = WebClient(token=os.environ.get("SLACK_BOT_TOKEN"))
+print(os.environ.get("SLACK_BOT_TOKEN"))
+logger = logging.getLogger(__name__)
+
+url = 'https://slack.com/api/chat.postMessage'
+
+# ID of channel you want to post message to
+
+channel_id = "C08UBDFST50"
+
+try:
+    # Call the conversations.list method using the WebClient
+    result = client.chat_postMessage(
+        channel=channel_id,
+        text="/taskstestagent Alice, pick some groceries for tomorrows thanksgiving event and send me the bill",
+        username='harveykisiangani',
+        icon_emoji=':white_check_mark:'
+        # You could also use a blocks[] array to send richer content
+    )
+    # Print result, which includes information about the message (like TS)
+    print(result)
+
+except SlackApiError as e:
+    print(f"Error: {e}")
+    

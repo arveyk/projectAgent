0b304d5240d572e09711264e2b090f507793f09c
Harvey Kisiagani <harveykisiangani@gmail.com>
2025-07-07 17:36:04 +0300

    Assync. send message to channel

--- projectAgent-Node/index.js
+++ projectAgent-Node/index.js
@@ -5,15 +5,21 @@
 const Slack = require('@slack/bolt');
 const { WebClient, LogLevel } = require("@slack/web-api");
 const token = process.env.SLACK_BOT_TOKEN
+const signingSecret = process.env.SLACK_SIGNING_SECRET,
+const nReadlines = require('n-readlines');
+
+const sampleChats = new nReadlines('../samplechats.txt')
+const channelID = "C08UBDFST50";
 
 const app = new Slack.App({
-  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  signingSecret: signingSecret,
   token: token
 });
 
 // WebClient instantiates a client that can call API methods
 // When using Bolt, you can use either `app.client` or the `client` passed to listeners.
-//
+
+
 const client = new WebClient(token, {
   // LogLevel can be imported and used to make debugging simpler
   logLevel: LogLevel.DEBUG
@@ -21,6 +27,7 @@ const client = new WebClient(token, {
 async function publishMessage(id, text) {
   try {
     // Call the chat.postMessage method using the built-in WebClient
+    
     const result = await app.client.chat.postMessage({
       // The token you used to initialize your app
       token: token,
@@ -28,6 +35,7 @@ async function publishMessage(id, text) {
       text: text
       // You could also use a blocks[] array to send richer content
     });
+
 
     // Print result, which includes information about the message (like TS)
     console.log(result);
@@ -37,5 +45,15 @@ async function publishMessage(id, text) {
   }
 }
 
-publishMessage("C093941T7JT", "Hello world :tada:");
+let line;
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+(async () => {
+  while (line = sampleChats.next()) {
+    await publishMessage(channelID, line.toString('ascii'));
+    await sleep(120000);
+  }
+})();
 

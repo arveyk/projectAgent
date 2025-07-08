b9a44d425a9466aaade5931d6c9f23ef413411b9
Ceci Kurdelak <crkurdelak@gmail.com>
2025-07-08 15:16:01 -0700

    Finished structured output demo script

--- projectAgent-Node/structured-output-demo.mjs
+++ projectAgent-Node/structured-output-demo.mjs
@@ -1,7 +1,7 @@
 import { ChatOpenAI } from "@langchain/openai";
 import { z } from "zod";
 
-OPENAI_API_KEY="";
+const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 
 const model = new ChatOpenAI({
   model: "gpt-4o-mini",
@@ -11,9 +11,10 @@ const model = new ChatOpenAI({
 const task = z.object({
     title: z.string().describe("The title of the task"),
     assignee: z.string().describe("The person the task is assigned to"),
-    start_date: z.date().describe("The start date of the task"),
-    due_date: z.date().describe("The due date of the task")
+    start_date: z.string().date().describe("The start date of the task"),
+    due_date: z.string().date().describe("The due date of the task")
 });
 
 const structuredLlm = model.withStructuredOutput(task, { name: "task" });
-await structuredLlm.invoke("Split this task into its fields. ### Example: Prompt: Pamela, could you work on the prototype for the new design starting July 8th? Please have it ready to present by the 16th. Result: title: Work on design prototype, assignee: Pamela, start_date: 2025-07-08, due_date: 2025-07-16 ### Michiko, please start editing the manuscript July 10, and try to have it finished by July 17")
+const result = await structuredLlm.invoke("Split this task into its fields. ### Example: Prompt: Pamela, could you work on the prototype for the new design starting July 8th? Please have it ready to present by the 16th. Result: title: Work on design prototype, assignee: Pamela, start_date: 2025-07-08, due_date: 2025-07-16 ### Michiko, please start editing the manuscript July 10, and try to have it finished by July 17");
+console.log(JSON.stringify(result));

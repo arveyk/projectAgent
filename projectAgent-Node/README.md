ecb958f48ae7d6be0457c6a50aab19d944105592
Ceci Kurdelak <crkurdelak@gmail.com>
2025-07-09 17:18:59 -0700

    Moved pa-newslackmessagetrigger to git repo

---
+++ projectAgent-Node/README.md
@@ -0,0 +1,4 @@
+# For local development:
+1. Install Functions Framework
+`npm install --save-dev @google-cloud/functions-framework`
+2. 
---
+++ projectAgent-Node/index.mjs
@@ -0,0 +1,14 @@
+import functions from '@google-cloud/functions-framework';
+
+functions.http('helloHttp', (req, res) => {
+  const request_params = req.body.request_params;
+  console.log(request_params);
+  res.send(`Hello ${req.query.name || req.body.name || 'World'}!\
+  user_id: ${req.body['user_id']}  \n\
+  channel_id: ${req.body['channel_id']} \n\
+  api_app_id: ${req.body['api_app_id']} \n\
+  channel_idL ${req.body['channel_id']} \n\
+  challenge: ${req.body['challenge']}  \n\
+  text: ${req.body['text']}  \n\
+  `);
+});

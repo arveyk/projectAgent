00d7b3d94ab4c03e877186200b771351116159a7
Ceci Kurdelak <crkurdelak@gmail.com>
2025-07-11 09:37:11 -0700

    Stubbed out test for helloHttp

---
+++ projectAgent-Node/index.test.js
@@ -0,0 +1,6 @@
+import helloHttp from './index.http';
+
+test('Responds to an HTTP request with a JSON string of its request parameters', () => {
+    // TODO send a mocked Slack request
+    // TODO check response
+})

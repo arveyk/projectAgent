09c896aa1992ef673a88b2ed07f40e5dde741642
Ceci Kurdelak <crkurdelak@gmail.com>
2025-07-11 21:48:11 -0700

    Made helloHttp send valid JSON response

--- projectAgent-Node/index.js
+++ projectAgent-Node/index.js
@@ -1,16 +1,18 @@
 import { http } from '@google-cloud/functions-framework';
+import { json } from 'express';
 
 http('helloHttp', (req, res) => {
   const request_params = req.body.request_params;
   console.log(request_params);
-  res.send(`Hello ${req.query.name || req.body.name || 'World'}!\
-  user_id: ${req.body['user_id']}  \n\
-  channel_id: ${req.body['channel_id']} \n\
-  api_app_id: ${req.body['api_app_id']} \n\
-  channel_idL ${req.body['channel_id']} \n\
-  challenge: ${req.body['challenge']}  \n\
-  text: ${req.body['text']}  \n\
-  `);
+  res.send(JSON.stringify({
+    greeting: `Hello ${req.query.name || req.body.name || 'World'}!`,
+    user_id: `${req.body['user_id']}`,
+    channel_id: `${req.body['channel_id']}`,
+    api_app_id: `${req.body['api_app_id']}`,
+    channel_idL: `${req.body['channel_id']}`,
+    challenge: `${req.body['challenge']}`,
+    text: `${req.body['text']}`
+  }));
 });
 
 

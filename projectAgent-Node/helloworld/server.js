0048f4dfc2bd697de6b1b03d872ceb323ff689d6
Harvey Kisiagani <harveykisiangani@gmail.com>
2025-07-16 15:54:45 +0300

    Slack bolt

--- projectAgent-Node/helloworld/server.js
+++ projectAgent-Node/helloworld/server.js
@@ -1,12 +1,56 @@
 import { json } from 'express';
 import express from 'express';
-import router from './index.js'
+import router from './index.js';
 import dotenv from 'dotenv';
+import App from '@slack/bolt';
+
+const { ExpressReceiver } = App; 
+
 dotenv.config();
 
-const app = express();
 const PORT = parseInt(process.env.PORT) || 8080;
 
+
+const homeHandler = {
+  path: '/',
+  method: ['ALL'],
+  handler: (request, response) => {
+    response.writeHead(200);
+    response.end(`Request Body content${
+      JSON.stringify(request.body)}`);
+  } 
+}
+
+const receiver = new ExpressReceiver({
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+});
+
+const slackApp = new App.App({
+  token: process.env.SLACK_BOT_TOKEN,
+  receiver,
+  customRoutes: [
+    homeHandler
+  ],
+});
+
+slackApp.event('message', async ({ event, client }) => {
+  await client.chat.postMessage('...');
+});
+
+receiver.router.use((request, response, next) => {
+  slackApp.logger.info(`Great timing! What is your request ${Date.now()}`);
+  next();
+});
+receiver.router.post('/events', (request, response) => {
+  response.send('Live event');
+});
+receiver.router.get('/home2', (request, response) => {
+  response.send('Home request');
+});
+
+//const app = express();
+
+/*
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
@@ -15,5 +59,12 @@ app.use(router);
 app.listen(PORT, () => {
   console.log(`Server's ears on port: ${PORT}`);
 });
+*/
 
-export { app };
+
+(async () => {
+  await slackApp.start(PORT);
+  slackApp.logger.info('⚡️ Bolt app is running!');
+})();
+
+export { slackApp };

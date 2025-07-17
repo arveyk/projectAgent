1cec4b771ec7174a22681d90b33389d192460829
Harvey Kisiagani <harveykisiangani@gmail.com>
2025-07-17 17:39:29 +0300

    Copied axios response to the eventsHandler

--- projectAgent-Node/routes/events.js
+++ projectAgent-Node/routes/events.js
@@ -1,7 +1,30 @@
+import axios from 'axios';
+
+
 const postHandler = function(request, response, next) {
     try {
       console.log(`I Handle most events. Any tasks for me?
 	 Request Body: ${JSON.stringify(request.body)}`);
+      const eventResURL = 'https://slack.com/api/chat.postMessage';
+      (async () => {
+        try {
+          if (request.body['api_app_id'] === undefined) {
+            const res = await axios.post(eventResURL, {
+              channel: '#task-management',
+              text: request.body['event']['text'],
+            }, {
+             headers: {
+               "Authorization": `Bearer ${process.env['SLACK_BOT_TOKEN']}`,
+               "Content-Type": "application/x-www-form-urlencoded",
+	     }
+	    });
+            console.log(res.data);
+	  }
+	} catch (err) {
+          console.error(err);
+        }
+      })();
+
     } catch (err){
         console.log(err);
         response.status(500).send(`Error and Body${request.body}`);

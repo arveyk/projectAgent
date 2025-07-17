c39cd2320ef495f9f45b6eaa07479848d534c891
Harvey Kisiagani <harveykisiangani@gmail.com>
2025-07-17 17:35:30 +0300

    eventsHandler called

--- projectAgent-Node/routes/events.js
+++ projectAgent-Node/routes/events.js
@@ -1,7 +1,7 @@
 const postHandler = function(request, response, next) {
     try {
       console.log(`I Handle most events. Any tasks for me?
-	 Request Body: ${JSON.stringify(request.body['challenge'])}`);
+	 Request Body: ${JSON.stringify(request.body)}`);
     } catch (err){
         console.log(err);
         response.status(500).send(`Error and Body${request.body}`);

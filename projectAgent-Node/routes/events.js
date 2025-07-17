76c01f9a5bd93596862f1d06919f9264ccf644c7
Ceci Kurdelak <crkurdelak@gmail.com>
2025-07-17 12:58:31 -0700

    Started message screening

--- projectAgent-Node/routes/events.js
+++ projectAgent-Node/routes/events.js
@@ -1,5 +1,26 @@
 import axios from 'axios';
+import dotenv from 'dotenv';
 
+dotenv.config();
+const APP_ID = process.env.PROJ_AGENT_APP_ID;
+
+/**
+ * Screens an incoming Slack message to see if it is a task assignment.
+ * @param {*} reqBody 
+ * @returns 
+ */
+const screenMessage = function(reqBody) {
+  // TODO check if message is a command
+  const isCommand = false;
+  // check if message is from Project Agent
+  // TODO find out why this is undefined
+  console.log(`app id: ${reqBody['api_app_id']}`)
+  const isFromProjAgent = (reqBody['api_app_id'] === APP_ID);
+  // TODO check if message is a task assignment
+  const isTask = true
+
+  return (!isCommand && !isFromProjAgent && isTask);
+}
 
 const postHandler = function(request, response, next) {
     try {
@@ -8,10 +29,13 @@ const postHandler = function(request, response, next) {
       const eventResURL = 'https://slack.com/api/chat.postMessage';
       (async () => {
         try {
-          if (request.body['api_app_id'] !== 'A08T4SJP659') {
+          if (screenMessage(request.body)) {
+            console.log("it's a task!");
+            console.log(`event: ${request.body['event']}`);
             const res = await axios.post(eventResURL, {
               channel: '#task-management',
-              text: request.body['event']['text'],
+              // TODO fix this line
+              text: request.body.jsonPayload['event']['text'],
             }, {
              headers: {
                "Authorization": `Bearer ${process.env['SLACK_BOT_TOKEN']}`,

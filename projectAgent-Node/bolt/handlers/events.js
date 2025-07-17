bebaea0266d7d9e681eeee843f1dfd35ef5a58fd
Harvey Kisiagani <harveykisiangani@gmail.com>
2025-07-17 13:41:35 +0300

    Added Logger to events, added channel to respond to for postMessage function

--- projectAgent-Node/bolt/handlers/events.js
+++ projectAgent-Node/bolt/handlers/events.js
@@ -1,7 +1,12 @@
 const ListenForEvents = (boltApp) => {
-  boltApp.event('message', async ({event, client}) => {
-    try{ 
-      const result = await client.chat.postMessage(`Hello <@${event}>New Message... Processing`);
+  boltApp.event('message', async ({event, client, logger}) => {
+    try{
+      logger.info("event info", event);
+      const result = await client.chat.postMessage({
+        channel: event.channel,
+	text: `Hello <@${event.user}>New Message... Processing`;
+      });
+      logger.info("Event result from Logger", result);
       console.log(result);
     } catch (err) {
       console.error(err);

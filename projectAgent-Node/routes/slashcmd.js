64036f34e8ce4f4b8dabab4d3477efb12a97b8d8
Harvey <harveykisiangani@gmail.com>
2025-07-23 13:43:03 +0300

    timely slash command

--- projectAgent-Node/routes/slashcmd.js
+++ projectAgent-Node/routes/slashcmd.js
@@ -2,7 +2,17 @@ const slashCmdHandler = function(request, response, next) {
     try {
       console.log(`slashCmdHandler here. Any tasks for me?
 	  Request Body: ${JSON.stringify(request.body)}`);
-      response.status(200).send(`Slash command ${request.body['event']['type']}`);
+      const command = request.body['command'];
+
+      const commandParams = request.body['text'].split(' ');
+      let firstArg = commandParams[0]
+      let otherArgs = commandParams.slice(1, -1).join(' ');
+
+      if (firstArg !== 'add'){
+        response.status(400).send(`Format: add ${request.body['text']}`);
+      } else {
+        response.status(200).send(`Correct format ${request.body['command']}`);
+      }
     } catch (err){
         console.log(err);
 	return response.status(404).send(err);

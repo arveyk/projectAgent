a1eecb26f2e7cef976cd25c6068a3a27c5107d73
Harvey <harveykisiangani@gmail.com>
2025-07-23 21:15:02 +0300

    sendBlock test successfull

--- projectAgent-Node/blockkit/sendBlockResponse.js
+++ projectAgent-Node/blockkit/sendBlockResponse.js
@@ -1,15 +1,28 @@
 import axios from 'axios';
 import { confirmationBlock, RequestApprovalBlock } from './sampleBlocks.js'
+import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
+dotenv.config({ path: path.join(__dirname, '..', '.env') });
+
+const webhookURL0 = process.env.TASK_MANAGEMENT_WEBHOOK_URL 
+const webhookURL1 = process.env.TASK_N8N_WEBHOOK_URL
+
+
+console.log(webhookURL0)
 //task management webhook url
-const webhookURL = "https://hooks.slack.com/services/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";
-
 axios({
   method: 'post',
-  url: webhookURL,
+  url: webhookURL0,
   data: confirmationBlock
 }).then((response) => {
-  console.log(response.body);
+  console.log(response);
 });
 
 

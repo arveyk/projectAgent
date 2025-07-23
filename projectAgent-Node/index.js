743e49fb163d63a900fc01727c3d020ec23f7306
Harvey <harveykisiangani@gmail.com>
2025-07-23 14:29:16 +0300

    Checks for events only

--- projectAgent-Node/index.js
+++ projectAgent-Node/index.js
@@ -2,14 +2,14 @@ import { json } from 'express';
 import express from 'express';
 import router from './middlerouter.js'
 import dotenv from 'dotenv';
-import bodyParser from 'body-parser'
+//import bodyParser from 'body-parser'
 
 dotenv.config();
 const PORT = parseInt(process.env.PORT) || 8080;
 
 const expressApp = express();
 
-expressApp.use(bodyParser.json());
+//expressApp.use(bodyParser.json());
 expressApp.use(express.urlencoded({extended: false}));
 expressApp.use(express.json());
 expressApp.use(router);
@@ -24,11 +24,12 @@ expressApp.get('/', (request, response) => {
   if (request.body) {
     console.log(`Welcome Home: Here\' an  object, JavaScript object
       ${JSON.stringify(request.body)}`);
+    response.status(200).send(`${JSON.stringify(request.body)}`);
   }
   else {
     console.log('your request has no body');
+    response.status(500).send(`Request Body contents: ${JSON.stringify(request.body)}`);
   }
-  response.status(200).send(`${JSON.stringify(request.body)}`);
 });
 
 expressApp.listen(PORT, () => {

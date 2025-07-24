47c0906d84c1cab9fba6451efeca886e193511d2
Ceci Kurdelak <crkurdelak@gmail.com>
2025-07-24 14:56:44 -0700

    Wrote tests for parseTask

--- projectAgent-Node/routes/events.test.js
+++ projectAgent-Node/routes/events.test.js
@@ -1,13 +1,27 @@
 import { app } from "../index.js";
 const request = require('supertest');
-import postHandler from './events';
+import screenMessage from './events';
 
 import payload_good from '../payloads/payload-good.json' with { type: 'json' };
+import payload_bad_from_app from '../payloads/payload-bad-from-app.json' with { type: 'json' };
 
 // TODO test screenMessage with task, non-task, 
 describe('Test screenMessage with a message containing a task assignment', () => {
-    it('', () => {
+    it('Returns true, along with the parsed task', () => {
+        const result = screenMessage(payload_good);
+        console.log(result);
+        expect(typeof result).toBe("object");
+        expect(result.isTask).toBeDefined;
+        expect(result.isTask).toBe(true);
+        expect(result.parsedTask).toBeDefined;
+    })
+})
 
+describe('Test screenMessage with a message containing no task assignment', () => {
+    it('Returns false', () => {
+        const result = screenMessage(payload_bad_from_app);
+        expect(typeof result).toBe("boolean");
+        expect(result).toBe(false);
     })
 })
 

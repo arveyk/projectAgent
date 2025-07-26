6e53323023ab0ef305ce0908eb0da828478bc350
Ceci Kurdelak <crkurdelak@gmail.com>
2025-07-25 17:10:17 -0700

    Created structured LLM to search database for matching tasks

--- projectAgent-Node/routes/db.js
+++ projectAgent-Node/routes/db.js
@@ -1,4 +1,6 @@
 import { Client } from '@notionhq/client';
+import { ChatAnthropic } from '@langchain/anthropic';
+import { z } from 'zod';
 
 import { 
   PORT, 
@@ -12,7 +14,18 @@ import {
 
 const notion = new Client({ auth: NOTION_API_KEY });
 
-// TODO define structured llm to check database result for matching tasks
+const model = new ChatAnthropic({
+  model: "claude-3-5-sonnet-20240620",
+  temperature: 0,
+  api_key: process.env["ANTHROPIC_API_KEY"]
+});
+
+const databaseSearchResult = z.object({
+  exists: z.boolean().describe("True if the task exists in the database, else false"),
+  task_id: z.string().optional().describe("The ID of the task entry from the database")
+})
+
+const structuredLlm = model.withStructuredOutput(databaseSearchResult);
 
 /**
  * Searches Notion database for a task based on its title and assignee fields
@@ -27,6 +40,13 @@ export const searchDB = async function(task) {
   console.log(`response: ${JSON.stringify(response)}`);
 
   // TODO have LLM determine if the task is there
+  const result = await structuredLlm.invoke(
+      `Please check if the task ${task} exists in the database response ${response} 
+      based on its title and assignee fields. If you find a task in the database response 
+      with the same assignee as the task you are searching for and a slightly different title 
+      that means the same thing as the title of the task you are searching for, this counts as a match.`
+  )
+  console.log(JSON.stringify(result));
 
-  return true;
+  return result;
 }
--- projectAgent-Node/routes/db.test.js
+++ projectAgent-Node/routes/db.test.js
@@ -1,16 +1,17 @@
 const request = require('supertest');
 import { searchDB } from './db';
+import { task } from './example-tasks';
 
 // TODO create example tasks for testing
 
 describe('Test searchDB with a task that is already in the database word for word', () => {
-    it('returns true', async () => {
-        await searchDB('hello');
+    it('returns true and the task ID from the database', async () => {
+        await searchDB();
     })
 })
 
 describe('Test searchDB with a task that is already in the database, but worded slightly differently', () => {
-    it('returns true', async () => {
+    it('returns true and the task ID from the database', async () => {
 
     })
 })

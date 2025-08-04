b710403dd561d9a90027db9a2707bd6e7e43ba75
Harvey <harveykisiangani@gmail.com>
2025-08-04 22:21:35 +0300

    Handle Different block separetely

--- projectAgent-Node/routes/paresponse/updateResponse.js
+++ projectAgent-Node/routes/paresponse/updateResponse.js
@@ -7,23 +7,27 @@ import {
 
 export default function testUpdateReply(request, response) {
   const payload = JSON.parse(request.body.payload);
-  //console.log(`Body: ${JSON.stringify(request.body)}`);
-  //console.log(`Body.payload${JSON.stringify(request.body.payload)}`);
+  console.log(`Body: ${JSON.stringify(request.body)}`);
+  console.log(`Body.payload${JSON.stringify(request.body.payload)}`);
   console.log('TRIGGER_ID', payload['trigger_id']);
   console.log(`RESPONSE URL ${(payload['response_url'])}`);
   console.log(`ACTIONS: ${JSON.stringify(payload['actions'])}`);
 
+  const trigger_id = payload['trigger_id'];
+  const response_url = payload['response_url'];
+  const message = payload['message'];
+  console.log(`TRIGGER_ID VARIABLE ${trigger_id}: RESPONSE_URL ${response_url} MESSAGE ${JSON.stringify(message)}`);
+	
   const action_id = payload['actions'][0]['action_id'];
   let action_text = "";
+  
   if (typeof payload['actions'][0]['selected_option'] !== 'undefined' ) {
     action_text = payload['actions'][0]['selected_option']['text']['text'];
-    console.log("In SELECTED OPTION");
-    /*
-     * if (action_text === "Approve") {
-        Task details
-        const taskDetailsObj =  JSON.parse(payload['actions'][0].value);
-        const createRowResult = addTaskNotionPage(taskDetailsObj); console.log(createRowResult);
-        const replaceBlockRes =  axios({
+    
+    if (action_text === "Approve") {
+      const taskDetailsObj =  JSON.parse(payload['actions'][0].value);
+      const createRowResult = addTaskNotionPage(taskDetailsObj); console.log(createRowResult);
+      const replaceBlockRes =  axios({
         method: "post",
         url: response_url,
         data: { 
@@ -38,85 +42,78 @@ export default function testUpdateReply(request, response) {
         console.log('Update msg',Response);
       }).catch((err) => {
         console.log(err);
-      });
-      }
-*/
+      }); 
+    }
 
       return "Edit Block Yet to be handled";
     } else {
       action_text = payload['actions'][0]['text']['text'];
       console.log("action_text in else block", action_text);
-    }
   
   
-  const trigger_id = payload['trigger_id'];
-  const response_url = payload['response_url'];
-  const message = payload['message'];
-  console.log(`TRIGGER_ID VARIABLE ${trigger_id}: RESPONSE_URL ${response_url} MESSAGE ${JSON.stringify(message)}`);
-	
-  if (action_text === "Approve") {
+      if (action_text === "Approve") {
     // Task details
-    const taskDetailsObj =  JSON.parse(payload['actions'][0]['value']);
+        const taskDetailsObj =  JSON.parse(payload['actions'][0]['value']);
 
 
-    const createRowResult = addTaskNotionPage(taskDetailsObj);
-    console.log(createRowResult);
+        const createRowResult = addTaskNotionPage(taskDetailsObj);
+        console.log(createRowResult);
 
-    const replaceBlockRes =  axios({
-      method: "post",
-      url: response_url,
-      data: { 
-	      "replace_original": "true",
-        "text": 'Block Replaced\nNotification: Task Created Successfully'
-      },
-      headers: {
-        "Authorization": `Bearer ${SLACK_BOT_TOKEN}`,
-        'Content-Type': 'application/json; charset=UTF-8',
-      }
-    }).then((Response) => {
-      console.log('Update msg',Response);
-    }).catch((err) => {
+        const replaceBlockRes =  axios({
+          method: "post",
+          url: response_url,
+          data: { 
+	    "replace_original": "true",
+            "text": 'Block Replaced\nNotification: Task Created Successfully'
+          },
+          headers: {
+            "Authorization": `Bearer ${SLACK_BOT_TOKEN}`,
+            "Content-Type": "application/json; charset=UTF-8",
+          }
+        }).then((Response) => {
+          console.log('Update msg',Response);
+        }).catch((err) => {
+          console.log(err);
+        });
+
+        response.status(200).send('Nice test');
+     } else if (action_text === "Edit") {
+       const taskDetailsObj =  JSON.parse(payload['actions'][0].value);
+       const block = createConfirmationBlock(taskDetailsObj);
+
+       const editResp =  axios({
+         method: "post",
+         url: response_url,
+         data: block,
+         headers: {
+          "Authorization": `Bearer ${SLACK_BOT_TOKEN}`,
+          "Content-Type": "application/json; charset=UTF-8",
+        }
+      }).then((Response) => {
+        console.log('Update msg',Response);
+      }).catch((err) => {
         console.log(err);
-    });
-
-    response.status(200).send('Nice test');
-  } else if (action_text === "Edit") {
-    const taskDetailsObj =  JSON.parse(payload['actions'][0].value);
-    const block = createConfirmationBlock(taskDetailsObj);
-
-
-    const editResp =  axios({
-      method: "post",
-      url: response_url,
-      data: block,
-      headers: {
-        "Authorization": `Bearer ${SLACK_BOT_TOKEN}`,
-        'Content-Type': 'application/json; charset=UTF-8',
-      }
-    }).then((Response) => {
-      console.log('Update msg',Response);
-    }).catch((err) => {
+      });
+      response.status(200).send('Nice test, Edit'); 
+    } else {
+      console.log(`Text in button ${payload.actions[0]['value']}, Action_Text${action_text}`);
+      const replaceBlockRes =  axios({
+        method: "post",
+        url: response_url,
+        data: { 
+          "replace_original": "true",
+          "text": 'Changes Not Approved: \n Please Note that these actions are only visible to you'
+        },
+        headers: {
+          "Authorization": `Bearer ${SLACK_BOT_TOKEN}`,
+          "Content-Type": "application/json; charset=UTF-8",
+        }
+      }).then((Response) => {
+        console.log('Update msg',Response);
+      }).catch((err) => {
         console.log(err);
-    });
-    response.status(200).send('Nice test, Edit'); 
-  } else {
-    console.log(`Text in button ${payload.actions[0]['value']}, Action_Text${action_text}`);
-    const replaceBlockRes =  axios({
-      method: "post",
-      url: response_url,
-      data: { 
-	      "replace_original": "true",
-        "text": 'Changes Not Approved: \n Please Note that these actions are only visible to you'
-      },
-      headers: {
-        "Authorization": `Bearer ${SLACK_BOT_TOKEN}`,
-        'Content-Type': 'application/json; charset=UTF-8',
-      }
-    }).then((Response) => {
-      console.log('Update msg',Response);
-    }).catch((err) => {
-        console.log(err);
-    });
-    response.status(200).send('Nice test, Discard', payload.actions[0]['value']); 
-  }
+      });
+      response.status(200).send('Nice test, Discard', payload.actions[0]['value']); 
+    }
+    }
 }

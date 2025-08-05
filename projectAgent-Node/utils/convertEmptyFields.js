41726bd3f0b233d8ce30ff0655b4224358b201fc
Ceci Kurdelak <crkurdelak@gmail.com>
2025-08-05 11:48:28 -0700

    Function documentation

--- projectAgent-Node/utils/convertEmptyFields.js
+++ projectAgent-Node/utils/convertEmptyFields.js
@@ -1,3 +1,8 @@
+/**
+ * Replaces "<UNKNOWN>" fields with empty strings.
+ * @param {*} task A task object
+ * @returns A task object with all "<UNKNOWN>" fields replaced with empty strings.
+ */
 export const convertEmptyFields = function(task) {
     let newTask = {
         tasktitle: "",

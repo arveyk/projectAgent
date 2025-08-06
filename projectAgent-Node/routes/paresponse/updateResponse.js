9cc07d6d82ba82cbff17562ceca61beea2ba1484
Harvey <harveykisiangani@gmail.com>
2025-08-06 22:54:41 +0300

    Have to revert

--- projectAgent-Node/routes/paresponse/updateResponse.js
+++ projectAgent-Node/routes/paresponse/updateResponse.js
@@ -118,7 +118,7 @@ export default function testUpdateReply(request, response, next) {
 	  }); 
 	}
       })();
-
+    }
           let replaceBlockRes;
           if (createRowResult) {
             replaceBlockRes = axios({

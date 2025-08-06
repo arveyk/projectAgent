209e6f3bb972d0bf0c6904d46e5916828bb2c5fb
Harvey <harveykisiangani@gmail.com>
2025-08-06 16:02:38 +0300

    Call row creation function asynchronously

--- projectAgent-Node/routes/paresponse/updateResponse.js
+++ projectAgent-Node/routes/paresponse/updateResponse.js
@@ -55,11 +55,14 @@ export default function testUpdateReply(request, response, next) {
     // Task details
       const taskDetailsObj =  JSON.parse(payload['actions'][0]['value']);
 
-      const createRowResult = addTaskNotionPage(taskDetailsObj)
+      let createRowResult;
+      (async () => {
+	createRowResult = await addTaskNotionPage(taskDetailsObj);
+      })();
       console.log(`CREATE ROW RESULT:${JSON.stringify(createRowResult)},\n ROW ID: ${createRowResult.id}`);
 
       let replaceBlockRes
-      if (createRowResult.id) {
+      if (createRowResult) {
 	replaceBlockRes =  axios({
           method: "post",
           url: response_url,

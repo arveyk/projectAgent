ffa33eb979505a0e22614213dfda450d10c44c52
Ceci Kurdelak <crkurdelak@gmail.com>
2025-08-07 10:27:46 -0700

    Removed tests for deleted /events endpoint

--- projectAgent-Node/utils/aiagent.test.js
+++ projectAgent-Node/utils/aiagent.test.js
@@ -1,5 +1,5 @@
 const request = require("supertest");
-import { screenMessage, parseTaskNewMsg, parseTaskSlashCmd } from "./aiagent";
+import { parseTaskSlashCmd } from "./aiagent";
 
 import payload_good from "../test-data/payloads/payload-good.json" with { type: "json" };
 import payload_bad_not_task from "../test-data/payloads/payload-bad-not-task.json" with { type: "json" };
@@ -9,77 +9,7 @@ import payload_good_feed_cats from "../test-data/payloads/payload-good-feed-cats
 import payload_good_infer_dates from "../test-data/payloads/payload-good-infer-dates.json" with { type: "json" };
 import payload_slash_cmd_infer_dates from "../test-data/payloads/payload-slash-cmd-infer-dates.json" with { type: "json" };
 
-describe("Test screenMessage with a message containing a task assignment", () => {
-  it("Returns true, along with the parsed task", async () => {
-    //console.log(JSON.stringify(payload_good));
-    expect(payload_good).toBeDefined;
-    expect(typeof payload_good).toBe("object");
-
-    const result = await screenMessage(payload_good);
-    console.log(`(in test) result: ${JSON.stringify(result)}`);
-    expect(typeof result).toBe("object");
-    expect(result.istask).toBeDefined;
-    expect(result.istask).toBe(true);
-    expect(result.parsedtask).toBeDefined;
-  });
-});
-
-describe("Test screenMessage with a message containing no task assignment", () => {
-  it("Returns false", async () => {
-    expect(payload_bad_not_task).toBeDefined;
-    expect(typeof payload_bad_not_task).toBe("object");
-
-    const result = await screenMessage(payload_bad_not_task);
-    console.log(`(in test) result: ${JSON.stringify(result)}`);
-    expect(typeof result).toBe("object");
-    expect(result.istask).toBeDefined;
-    expect(result.istask).toBe(false);
-  });
-});
-
-describe("Test screenMessage with a message sent by a bot", () => {
-  it("Returns false", async () => {
-    //console.log(JSON.stringify(payload_bad_from_app));
-    expect(payload_bad_from_app).toBeDefined;
-    expect(typeof payload_bad_from_app).toBe("object");
-
-    const result = await screenMessage(payload_bad_from_app);
-    console.log(`(in test) result: ${JSON.stringify(result)}`);
-    expect(typeof result).toBe("object");
-    expect(result.istask).toBeDefined;
-    expect(result.istask).toBe(false);
-  });
-});
-
-describe("Test screenMessage with a channel join message", () => {
-  it("Returns false", async () => {
-    //console.log(JSON.stringify(payload_bad_from_app));
-    expect(payload_bad_channel_join).toBeDefined;
-    expect(typeof payload_bad_channel_join).toBe("object");
-
-    const result = await screenMessage(payload_bad_channel_join);
-    console.log(`(in test) result: ${JSON.stringify(result)}`);
-    expect(typeof result).toBe("object");
-    expect(result.istask).toBeDefined;
-    expect(result.istask).toBe(false);
-  });
-});
-
-describe("Test screenMessage with the message that broke production", () => {
-  it("Returns true, along with the parsed task", async () => {
-    //console.log(JSON.stringify(payload_good));
-    expect(payload_good_feed_cats).toBeDefined;
-    expect(typeof payload_good_feed_cats).toBe("object");
-
-    const result = await screenMessage(payload_good_feed_cats);
-    console.log(`(in test) result: ${JSON.stringify(result)}`);
-    expect(typeof result).toBe("object");
-    expect(result.istask).toBeDefined;
-    expect(result.istask).toBe(true);
-    expect(result.parsedtask).toBeDefined;
-  });
-});
-
+// TODO test slash command functions
 describe("Tests parseTaskNewMsg inferring dates", () => {
   it("Infers start date and due date correctly", async () => {
     expect(payload_good_infer_dates).toBeDefined;

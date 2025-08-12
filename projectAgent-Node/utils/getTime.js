911ec996df065e9ccd3aab525aca9046b7e6387a
Ceci Kurdelak <crkurdelak@gmail.com>
2025-08-11 20:41:19 -0700

    Returned timezone data

--- projectAgent-Node/utils/getTime.js
+++ projectAgent-Node/utils/getTime.js
@@ -1,6 +1,14 @@
 import axios from "axios";
 import { SLACK_BOT_TOKEN } from "../env.js";
 
+const SECONDS_IN_MINUTE = 60;
+const MINUTES_IN_HOUR = 60;
+
+/**
+ * Gets timezone data about a user.
+ * @param {*} userID The user's ID
+ * @returns The user's timezone, timezone label, and offset from UTC
+ */
 export const getUserTimezone = async function(userID) {
     const resp = await axios({
         method: "get",
@@ -10,6 +18,10 @@ export const getUserTimezone = async function(userID) {
               Authorization: `Bearer ${SLACK_BOT_TOKEN}`,
             },
     });
-    // TODO get timezone
-    console.log(JSON.stringify(resp.data));
+    const userData = resp.data["user"];
+    return {
+        tz: userData["tz"],
+        tz_label: userData["tz_label"],
+        tz_offset: userData["tz_offset"] / (SECONDS_IN_MINUTE * MINUTES_IN_HOUR)
+    }
 }
--- projectAgent-Node/utils/getTime.test.js
+++ projectAgent-Node/utils/getTime.test.js
@@ -1,8 +1,17 @@
 import { getUserTimezone } from "./getTime";
+import dotenv from 'dotenv'
+dotenv.config()
+const userID = process.env.TEST_USER_ID;
 
 describe("Tests getUserTimezone with a user from the workspace", () => {
     it("Returns the user's timezone", async () => {
-        const userID = "U08VADHNG0G";
-        await getUserTimezone(userID);
+        const timezone = await getUserTimezone(userID);
+        console.log(JSON.stringify(timezone));
+
+        expect(timezone).toMatchObject({
+            tz: "America/Los_Angeles",
+            tz_label: "Pacific Daylight Time",
+            tz_offset: -7
+        })
     })
 })

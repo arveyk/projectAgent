a06b20cdeae8f839bfd2ed17d11fc0e952c5c722
arveyk <harveykisiangani@gmail.com>
2025-09-04 17:18:38 +0300

    Search user: tests

---
+++ projectAgent-Node/utils/controllers/sample.js
@@ -0,0 +1,29 @@
+export const users = [
+  {
+    name: "@Danil D",
+    email: "daniel@solul.com",
+  },
+  {
+    name: "Bill Jones",
+    email: "billjones@gmail.com",
+  },
+  {
+    name: "@Ceci Kurdelak",
+    email: "@ceci.kurdk@solal.com",
+  },
+  {
+    name: "@Nick Furry",
+    email: "nick@shield.com"
+  },
+  {
+    name: "@Nick Furry",
+    email: "nickfurry@shield.com"
+  }
+];
+
+export const task = {
+  assignee: "Nick Furry",
+  email: "nick@shield.com",
+  tasktitle: "Avengers Initiative",
+  due: "20-04-2025",
+};

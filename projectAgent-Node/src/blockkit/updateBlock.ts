c73b837c11bf457e75337ccded8667540ffdaf41
arveyk <harveykisiangani@gmail.com>
2025-09-30 20:39:57 +0300

    Changed extension to .ts

---
+++ projectAgent-Node/src/blockkit/updateBlock.ts
@@ -0,0 +1,52 @@
+export function createUpdateBlock(task) {
+  const startDate = task.startdate.split("T")[0];
+  const dueDate = task.duedate.split("T")[0];
+
+  task.startdate = startDate;
+  task.duedate = dueDate;
+  return {
+    blocks: [
+      {
+        type: "header",
+        text: {
+          type: "plain_text",
+          text: "Task already exists, would you like to update it?",
+          emoji: true,
+        },
+      },
+      {
+        type: "section",
+        text: {
+          type: "mrkdwn",
+          text: `*Task title*:\t\t\t\t\t\t\t ${task.tasktitle} \n *Assignee*  \t\t\t\t\t\t\t${task.assignee}\n* Due Date*  \t\t\t\t\t\t  ${dueDate}\n*Start Date*  \t\t\t\t\t\t  ${startDate}\n*Email:* \t\t\t\t\t\t\t\t  ${task.email} \n*Phone Number:* \t\t\t\t${task.phonenumber}\n *Preferred Channel:* \t\t\t${task.preferredchannel} \n*Task Details:*\t\t\t\t\t ${task.description}\n*Project:*\t\t\t\t\t\t${task.project}\n*Task Link* \t\t\t\t\t\t\t<${task.url}|link>`,
+        },
+      },
+      {
+        type: "actions",
+        elements: [
+          {
+            type: "button",
+            text: {
+              type: "plain_text",
+              text: "No",
+              emoji: true,
+            },
+            value: "click_me_123",
+            action_id: "actionId-0",
+          },
+          {
+            type: "button",
+            text: {
+              type: "plain_text",
+              text: "Yes",
+              emoji: true,
+            },
+            value: JSON.stringify(task),
+            action_id: "actionId-1",
+            style: "primary",
+          },
+        ],
+      },
+    ],
+  };
+}

efd36997ba6a58278a5dc6755b8d2be68a8fa9bc
arveyk <harveykisiangani@gmail.com>
2025-10-02 19:34:18 +0300

    Used db-update as template page properties set up

--- projectAgent-Node/src/utils/setTaskProperties.ts
+++ projectAgent-Node/src/utils/setTaskProperties.ts
@@ -1,73 +1,79 @@
-import { validateDate } from "./dateHandler";
-import { Task } from "./task";
+import de from "zod/dist/types/v4/locales/de.js";
+import { task } from "./controllers/someTypes.js";
+import { validateDate } from "./dateHandler.js";
+import { Task } from "./task.js";
 
 const setTitleArray = function (taskTitle: string) {
   return [
     {
-      type: "text",
       text: {
-        content: taskTitle,
-        link: null,
-      },
-      annotations: {
-        bold: false,
-        italic: false,
-        strikethrough: false,
-        underline: false,
-        code: false,
-        color: "default",
-      },
-      plain_text: "Title of the task",
-      href: null,
-    },
-  ];
+        content: taskTitle
+      }
+    }];
 };
 const setAssigneeArray = function (assignee: string) {
   return [
     {
-      type: "text",
       text: {
         content: assignee,
-        link: null,
       },
-      annotations: {
-        bold: false,
-        italic: false,
-        strikethrough: false,
-        code: false,
-        color: "default",
-      },
-      href: null,
+    }
+    /*
+    annotations: {
+      bold: false,
+      italic: false,
+      strikethrough: false,
+      code: false,
+      color: "default",
     },
+    href: null,
+    */
   ];
 };
 const setPreferredChannelArray = function (preferredChannel: string) {
   return [
     {
-      type: "text",
       text: {
-        content: preferredChannel,
-        link: null,
+        content: preferredChannel
       },
-      annotations: {
-        bold: false,
-        italic: false,
-        strikethrough: false,
-        code: false,
-        color: "default",
-      },
-      href: null,
     },
-  ];
+  ]
+  /*{
+    type: "text",
+    text: {
+      content: preferredChannel,
+      link: null,
+    },
+    
+    annotations: {
+      bold: false,
+      italic: false,
+      strikethrough: false,
+      code: false,
+      color: "default",
+    },
+    href: null,
+   
+  }, */
+
+
 };
 const setDescriptionArray = function (description: string) {
   return [
+    {
+      text: {
+        content: description,
+      },
+    },
+  ]
+  /*[
     {
       type: "text",
       text: {
         content: description,
         link: null,
       },
+
       annotations: {
         bold: false,
         italic: false,
@@ -77,17 +83,18 @@ const setDescriptionArray = function (description: string) {
         color: "default",
       },
       href: null,
-    },
+
+    },  
   ];
+  */
 };
 const setProjectArray = function (project: string) {
   return [
     {
-      type: "text",
       text: {
         content: project,
-        link: null,
       },
+      /*
       annotations: {
         bold: false,
         italic: false,
@@ -96,18 +103,18 @@ const setProjectArray = function (project: string) {
         color: "default",
       },
       href: null,
+      */
     },
   ];
 };
 const setAssignedByArray = function (assignedBy: string) {
-// const setAssignedByArray = function (assignedBy: {name: string, email: string}[]) {
+  // const setAssignedByArray = function (assignedBy: {name: string, email: string}[]) {
   return [
     {
-      type: "text",
       text: {
         content: assignedBy,
-        link: null,
       },
+      /*
       annotations: {
         bold: false,
         italic: false,
@@ -115,21 +122,22 @@ const setAssignedByArray = function (assignedBy: string) {
         code: false,
         color: "default",
       },
+      */
       href: null,
     },
   ];
 };
 export const setTaskProperties = function (taskObj: Task, assignedBy: string) {
-// export const setTaskProperties = function (taskObj: Task, assignedBy: {name: string, email: string}[]) {
+  // export const setTaskProperties = function (taskObj: Task, assignedBy: {name: string, email: string}[]) {
   const taskTitle = taskObj["taskTitle"];
   const assignee = taskObj["assignee"];
   const dueDate = validateDate(taskObj["dueDate"].toISOString());
   const startDate =
-    taskObj["startDate"] ? validateDate(taskObj["startDate"].toISOString()): new Date().toISOString();
-    // taskObj["startDate"] !== ""
-    //? new Date(taskObj["startdate"]).toISOString()
-    //    new validateDate(taskObj["startDate"]).toISOString()
-    //  : new Date().toISOString();
+    taskObj["startDate"] ? validateDate(taskObj["startDate"].toISOString()) : new Date().toISOString();
+  // taskObj["startDate"] !== ""
+  //? new Date(taskObj["startdate"]).toISOString()
+  //    new validateDate(taskObj["startDate"]).toISOString()
+  //  : new Date().toISOString();
   const email = taskObj["email"] || " ";
   const phoneNumber = taskObj["phoneNumber"] || " ";
   const preferredChannel = taskObj["preferredChannel"] || "Slack";
@@ -139,53 +147,42 @@ export const setTaskProperties = function (taskObj: Task, assignedBy: string) {
 
   return {
     "Task Title": {
-      type: "title",
       title: setTitleArray(taskTitle),
     },
     Assignee: {
-      type: "rich_text",
       rich_text: setAssigneeArray(assignee),
     },
     "Due Date": {
-      type: "date",
       date: {
-        start: dueDate,
+        start: dueDate.toString(),
       },
     },
     "Start Date": {
-      type: "date",
       date: {
-        start: startDate,
+        start: startDate.toString(),
       },
     },
     "Phone Number": {
-      type: "phone_number",
       phone_number: phoneNumber,
     },
     Email: {
-      type: "email",
       email: email,
     },
     "Preferred Channel": {
-      type: "rich_text",
       rich_text: setPreferredChannelArray(preferredChannel),
     },
     Description: {
-      type: "rich_text",
       rich_text: setDescriptionArray(description),
     },
     "Date Assigned": {
-      type: "date",
       date: {
         start: dateAssigned,
       },
     },
     Project: {
-      type: "rich_text",
       rich_text: setProjectArray(project),
     },
     "Assigned By": {
-      type: "rich_text",
       rich_text: setAssignedByArray(assignedBy),
     },
   };

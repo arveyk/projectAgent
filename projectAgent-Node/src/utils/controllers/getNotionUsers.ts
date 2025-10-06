414d6bbade9883635655875737e6e4808ab98835
Ceci Kurdelak <crkurdelak@gmail.com>
2025-10-06 09:37:50 -0700

    Fixed import

--- projectAgent-Node/src/utils/controllers/getNotionUsers.ts
+++ projectAgent-Node/src/utils/controllers/getNotionUsers.ts
@@ -1,6 +1,6 @@
 import { User } from "./someTypes.js";
 import { ListUsersResponse } from "@notionhq/client/build/src/api-endpoints.js";
-import { NOTION_API_KEY } from "../../env.js";
+import { NOTION_API_KEY } from "../../env";
 import { Client} from "@notionhq/client";
 
 const notion = new Client({

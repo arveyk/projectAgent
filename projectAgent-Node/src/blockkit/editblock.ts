96d1bde6587457360ff7c130f5e7c746477a3f9a
arveyk <harveykisiangani@gmail.com>
2025-10-08 18:19:04 +0300

    Send Date object to formatSlackDate

--- projectAgent-Node/src/blockkit/editblock.ts
+++ projectAgent-Node/src/blockkit/editblock.ts
@@ -9,7 +9,7 @@ export const createTaskInfoBlock = function (task: Task) {
   // task.dueDate = new Date(task.dueDate);
   task.startDate = task.startDate && task.startDate.toString() !== "Invalid Date" ? new Date(task.startDate) : DateTime.now().toJSDate();
   console.log(`CreateTaskInfoBlock log message => task: ${task}`);
-  return `*Task Title:*\t\t\t${task.taskTitle} \n*Assignee:* \t\t\t${task.assignee}\n*Due Date:*\t\t\t${formatSlackDate(task.dueDate)}\n*Start Date:*\t\t\t${(task.startDate !== new Date(NaN) && task.startDate !== undefined) ? formatSlackDate(task.startDate) : task.startDate}\n*Phone Number:*\t${task.phoneNumber}\n*Email:*\t\t\t${task.email}\n*Preferred Channel:*\t\t\t${task.preferredChannel}\n*Description:* \t\t${task.description}\n*Project:* \t\t${task.project}`;
+  return `*Task Title:*\t\t\t${task.taskTitle} \n*Assignee:* \t\t\t${task.assignee}\n*Due Date:*\t\t\t${formatSlackDate(new Date(task.dueDate))}\n*Start Date:*\t\t\t${(task.startDate !== new Date(NaN) && task.startDate !== undefined) ? formatSlackDate(task.startDate) : task.startDate}\n*Phone Number:*\t${task.phoneNumber}\n*Email:*\t\t\t${task.email}\n*Preferred Channel:*\t\t\t${task.preferredChannel}\n*Description:* \t\t${task.description}\n*Project:* \t\t${task.project}`;
 };
 
 export function createEditBlock(task: Task) {

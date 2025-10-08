22e56758898b585f2906209ab711217af3e7c32b
Ceci Kurdelak <crkurdelak@gmail.com>
2025-10-08 12:31:01 -0700

    Fixed ternary expressions

--- projectAgent-Node/src/utils/task.ts
+++ projectAgent-Node/src/utils/task.ts
@@ -68,36 +68,42 @@ export function convertTaskPageFromDbResponse(pageResponse: PageObjectResponse):
   const properties = pageResponse["properties"];
 
   const title = "title" in properties["Task Title"]
-          ? properties["Task Title"].title[0].plain_text
-          : "No Title Provided";
-  const assignee =  "rich_text" in properties.Assignee
-          ? properties["Assignee"].rich_text[0].plain_text
-          : "No Assignee";
+    ? properties["Task Title"].title[0].plain_text
+    : "No Title Provided";
+  const assignee = "rich_text" in properties.Assignee
+    ? properties["Assignee"].rich_text[0].plain_text
+    : "No Assignee";
   const dueDate = "date" in properties["Due Date"]
-          ? properties["Due Date"].date
-            ? new Date(properties["Due Date"].date.start)
-            : new Date()
-          : new Date();
+    ? properties["Due Date"].date
+      ? new Date(properties["Due Date"].date.start)
+      : new Date()
+    : new Date();
   const startDate = "date" in properties["Start Date"]
-          ? properties["Start Date"].date
-            ? new Date(properties["Start Date"].date.start)
-            : new Date()
-          : new Date();
+    ? properties["Start Date"].date
+      ? new Date(properties["Start Date"].date.start)
+      : new Date()
+    : new Date();
   const email = "email" in properties["Email"]
-          ? properties["Email"].email || undefined
-          : undefined;
+    ? properties["Email"].email || undefined
+    : undefined;
   const phoneNumber = "phone_number" in properties["Phone Number"]
-          ? properties["Phone Number"].phone_number || undefined
-          : undefined;
-  const preferredChannel = ("rich_text" in properties["Preferred Channel"] && "plain_text" in properties["Preferred Channel"]["rich_text"][0])
-          ? properties["Preferred Channel"].rich_text[0].plain_text
-          : "";
-  const description = ("rich_text" in properties["Description"] && "plain_text" in properties["Description"]["rich_text"][0])
-          ? properties["Description"].rich_text[0].plain_text
-          : "";
-  const project = ("rich_text" in properties["Project"] && "plain_text" in properties["Project"]["rich_text"][0])
-          ? properties["Project"].rich_text[0].plain_text
-          : "";
+    ? properties["Phone Number"].phone_number || undefined
+    : undefined;
+  const preferredChannel = "rich_text" in properties["Preferred Channel"] 
+  ? "plain_text" in properties["Preferred Channel"]["rich_text"][0]
+    ? properties["Preferred Channel"].rich_text[0].plain_text
+    : ""
+  : "";
+  const description = "rich_text" in properties["Description"] 
+  ? "plain_text" in properties["Description"]["rich_text"][0]
+    ? properties["Description"].rich_text[0].plain_text
+    : ""
+  : "";
+  const project = "rich_text" in properties["Project"] 
+  ? "plain_text" in properties["Project"]["rich_text"][0]
+    ? properties["Project"].rich_text[0].plain_text
+    : ""
+  : "";
   const url = pageResponse.url;
   const pageId = pageResponse.id;
 
@@ -106,7 +112,7 @@ export function convertTaskPageFromDbResponse(pageResponse: PageObjectResponse):
       taskTitle:
         title,
       assignee:
-       assignee,
+        assignee,
       dueDate:
         dueDate,
       startDate:

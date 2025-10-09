a6295ee99e446aaf3a5d3ad4948b17293738799e
Ceci Kurdelak <crkurdelak@gmail.com>
2025-10-09 09:56:32 -0700

    Called the correct function

--- projectAgent-Node/src/utils/dateHandler.ts
+++ projectAgent-Node/src/utils/dateHandler.ts
@@ -39,7 +39,7 @@ export const validateDueDate = function (dueDate: Date): boolean {
   // new Date(taskInput["dueDate"])
   const today: Date = new Date();
   //const today = DateTime.now().setZone("utc").toJSDate();
-  console.log(`(validateDueDate) Today: ${today} (${today.getUTCMilliseconds()} millis) Due Date: ${dueDate} (${dueDate.getUTCMilliseconds()} millis)`);
+  console.log(`(validateDueDate) Today: ${today} (${today.getTime()} millis) Due Date: ${dueDate} (${dueDate.getTime()} millis)`);
 
   return dueDate >= today;
 };

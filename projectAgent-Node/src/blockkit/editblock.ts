97b3874e2b3d865a6bc7794a84ecbd6c2f4951e4
arveyk <harveykisiangani@gmail.com>
2025-10-20 17:46:18 +0300

    check if assignee name is defined

--- projectAgent-Node/src/blockkit/editblock.ts
+++ projectAgent-Node/src/blockkit/editblock.ts
@@ -12,9 +12,11 @@ export const createTaskInfoBlock = function (taskPageObj: TaskPage) {
   let assigneeNames = "";
 
   console.log(`(createTaskInfoBlock), assigneesArray: ${assigneesArr}, task${JSON.stringify(task)}`);
-  if (assigneesArr && Array.isArray(assigneesArr)) {
+  if (assigneesArr && Array.isArray(assigneesArr))
     assigneesArr.forEach((assignee) => {
-      assigneeNames += `${assignee.name}, `;
+      if (assignee) {
+	      assigneeNames += `${assignee.name}, `;
+      }
     });
     assigneeNames = assigneeNames.slice(0, -2); // Remove trailing comma and space
   }

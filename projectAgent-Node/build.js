4b626ca7863cbd98340a2afb982393a793e4b309
Ceci Kurdelak <crkurdelak@gmail.com>
2025-10-23 15:59:18 -0700

    Build with esbuild

---
+++ projectAgent-Node/build.js
@@ -0,0 +1,10 @@
+import { build } from "esbuild";
+
+build({
+    entryPoints: ["src/index.ts"],
+    bundle: true,
+    minify: true,
+    treeShaking: true,
+    platform: 'node',
+    outfile: "dist/index.js",
+});

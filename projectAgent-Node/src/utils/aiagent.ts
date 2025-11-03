026482c631c93e4c49b4238faa159629b5e4b26b
arveyk <harveykisiangani@gmail.com>
2025-11-03 16:07:06 +0300

    Update model to 4-5 sonnet

--- projectAgent-Node/src/utils/aiagent.ts
+++ projectAgent-Node/src/utils/aiagent.ts
@@ -10,7 +10,7 @@ import { SlashCommand } from "@slack/bolt";
 import { logTime } from "./logTime";
 
 const model = new ChatAnthropic({
-  model: "claude-3-5-sonnet-20240620",
+  model: "claude-sonnet-4-5-20250929",
   temperature: 0,
   apiKey: ANTHROPIC_API_KEY,
 });
--- projectAgent-Node/src/utils/db-search.ts
+++ projectAgent-Node/src/utils/db-search.ts
@@ -20,7 +20,7 @@ const notion = new Client({
 });
 
 const model = new ChatAnthropic({
-  model: "claude-3-5-sonnet-20240620",
+  model: "claude-sonnet-4-5-20250929",
   temperature: 0,
   apiKey: ANTHROPIC_API_KEY,
 });

c39ed6ae154c20db0003fe1597895fd452ea116d
Ceci Kurdelak <crkurdelak@gmail.com>
2026-02-06 12:26:12 -0800

    Parametrized the function that updates an individual cache item, and finished implementing cache updating

--- projectAgent-RefreshCache/src/routes/refreshCache.ts
+++ projectAgent-RefreshCache/src/routes/refreshCache.ts
@@ -8,14 +8,14 @@ const cacheRefreshHandler: Handler = async function (
     event: APIGatewayProxyEventV2,
     context: Context
 ) {
-    console.log("Hello world");
+    console.log("Refreshing cache");
 
     const taskResponse = await getTasksRaw();
     const projectResponse = await getProjectsRaw();
     const userResponse = await getUsers();
 
     const cachRefreshResult = await refreshCache(taskResponse, projectResponse, userResponse);
-
+    return cachRefreshResult;
 }
 
 export default cacheRefreshHandler;
--- projectAgent-RefreshCache/src/utils/dynamoDB.ts
+++ projectAgent-RefreshCache/src/utils/dynamoDB.ts
@@ -1,5 +1,5 @@
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
-import { BatchGetCommand, BatchGetCommandOutput, DynamoDBDocumentClient, PutCommand, UpdateCommand } from "@aws-sdk/lib-dynamodb";
+import { BatchGetCommand, BatchGetCommandOutput, DynamoDBDocumentClient, PutCommand, UpdateCommand, UpdateCommandOutput } from "@aws-sdk/lib-dynamodb";
 import { ListUsersResponse, QueryDataSourceResponse } from "@notionhq/client";
 import { CACHE_TABLE_NAME, REGION } from "../env";
 
@@ -13,11 +13,11 @@ const DDBDocumentClient: DynamoDBDocumentClient = DynamoDBDocumentClient.from(DD
  * @param projectResponse The raw project response from Notion.
  * @param userResponse The raw user response from Notion.
  */
-export async function refreshCache(taskResponse: QueryDataSourceResponse["results"], projectResponse: QueryDataSourceResponse["results"], userResponse: ListUsersResponse) {
+export async function refreshCache(taskResponse: QueryDataSourceResponse["results"], projectResponse: QueryDataSourceResponse["results"], userResponse: ListUsersResponse): Promise<CacheRefreshResponse> {
     // TODO compress the data before storing it
 
-    // check if the entry exists
-    // Fetch all items
+    // Fetch all items so we can check which entries already exist (needs "update" request)
+    // and which were deleted (needs "put" request)
     const getCommand = new BatchGetCommand({
         RequestItems: {
             'projectagent-cache-dev': {
@@ -29,31 +29,38 @@ export async function refreshCache(taskResponse: QueryDataSourceResponse["result
             }
         }
     })
-
     const batchGetResponse = await DDBDocumentClient.send(getCommand);
     console.log(JSON.stringify(batchGetResponse));
-    const taskCacheResponse = await refreshTaskCache(taskResponse, batchGetResponse);
-    console.log(JSON.stringify(taskCacheResponse));
+
+    // Refresh the cache
+    const taskCacheResponse = await refreshCacheItem("task", taskResponse, batchGetResponse);
+    const projectCacheResponse = await refreshCacheItem("project", projectResponse, batchGetResponse);
+    const userCacheResponse = await refreshCacheItem("user", userResponse, batchGetResponse);
+    const response = {
+        task: taskCacheResponse,
+        project: projectCacheResponse,
+        user: userCacheResponse
+    };
+    return response;
 }
 
 /**
- * Updates the task cache.
- * @param taskResponse The raw task response from Notion.
+ * Updates a cache item.
+ * @param itemType The type of item being updated in the cache (task, project, or user)
+ * @param taskResponse The raw response from Notion.
  * @param batchGetResponse The response from getting the current cache values.
  * @returns The response from updating the cache.
  */
-async function refreshTaskCache(taskResponse: QueryDataSourceResponse["results"], batchGetResponse: BatchGetCommandOutput) {
-
+async function refreshCacheItem(itemType: "task" | "project" | "user", taskResponse: QueryDataSourceResponse["results"] | ListUsersResponse, batchGetResponse: BatchGetCommandOutput): Promise<UpdateCommandOutput> {
     const batchGetResponses = batchGetResponse.Responses;
 
     let cacheResponse;
-    // if it exists, update it
-    if (batchGetResponses && "task" in batchGetResponses) {
-        // TODO fix the update command
+    // If the cache entry exists, update it
+    if (batchGetResponses && itemType in batchGetResponses) {
         const command = new UpdateCommand({
             TableName: CACHE_TABLE_NAME,
             Key: {
-                "ItemType": "Task"
+                "ItemType": itemType
             },
             AttributeUpdates: {
                 "RawResponse": {
@@ -64,12 +71,12 @@ async function refreshTaskCache(taskResponse: QueryDataSourceResponse["results"]
         })
         cacheResponse = await DDBDocumentClient.send(command);
     }
-    // if it doesn't exist, create it
+    // If the cache entry doesn't exist, create it
     else {
         const command = new PutCommand({
             TableName: CACHE_TABLE_NAME,
             Item: {
-                "ItemType": "task",
+                "ItemType": itemType,
                 "RawResponse": taskResponse
             }
         });
@@ -78,3 +85,12 @@ async function refreshTaskCache(taskResponse: QueryDataSourceResponse["results"]
 
     return cacheResponse;
 }
+
+/**
+ * A wrapper type for the DynamoDB responses from refreshing the cache.
+ */
+export type CacheRefreshResponse = {
+    task: UpdateCommandOutput,
+    project: UpdateCommandOutput,
+    user: UpdateCommandOutput
+};

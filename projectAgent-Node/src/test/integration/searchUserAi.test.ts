e9b006a37c43ff0d46ce68c5cfc61290244de274
arveyk <harveykisiangani@gmail.com>
2026-02-10 18:38:55 +0300

    Test: If user is in Notion

--- projectAgent-Node/src/test/integration/searchUserAi.test.ts
+++ projectAgent-Node/src/test/integration/searchUserAi.test.ts
@@ -4,7 +4,7 @@ import { EXAMPLE_ALL_NOTION_USERS } from "../../test-data/example-all-notion-use
 import { taskHarvey
  } from "../../test-data/tasks/example-tasks";
 describe("Seach with ai", () => {
-  it("Should return result with value false if user in not in Users", async () => {
+  it("Should return result with value true if user is in Notion", async () => {
     const res = await searchUser(taskHarvey, EXAMPLE_ALL_NOTION_USERS);
     expect(res.found).toBe(true);
   });

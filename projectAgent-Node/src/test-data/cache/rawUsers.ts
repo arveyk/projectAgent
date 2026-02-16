5a2a27f79fc97d038d01d86c58cfec8302399192
arveyk <harveykisiangani@gmail.com>
2026-02-16 14:04:29 +0300

    Obfuscate data

--- projectAgent-Node/src/test-data/cache/rawUsers.ts
+++ projectAgent-Node/src/test-data/cache/rawUsers.ts
@@ -6,221 +6,221 @@ export const EXAMPLE_RAW_USERS_RESPONSE: ListUsersResponse = {
     {
       "object": "user",
       "id": "13ed872b-594c-81a7-84f8-0002e9e2cf87",
-      "name": "Drew Dunn",
-      "avatar_url": "https://lh3.googleusercontent.com/a/ACg8ocLyodOzCTjEDcccXje7YYMPNR4hox_YgXLnjiglm0qbRuFlkQ=s100",
+      "name": "Drew Michaels",
+      "avatar_url": "placeholder_avatar_url",
       "type": "person",
       "person": {
-        "email": "drew.dunn@solutional.com"
+        "email": "drew.michaels@example-domain.com"
       }
     },
     {
       "object": "user",
       "id": "152d872b-594c-8145-9c2c-000204787b69",
-      "name": "Ceci Kurdelak",
-      "avatar_url": "https://s3-us-west-2.amazonaws.com/public.notion-static.com/78f66135-d1a4-4c17-8217-4e025621539c/20240104_175839_-_Copy_(2).jpg",
+      "name": "Dresdan Tabitha",
+      "avatar_url": "placeholder_sample_url",
       "type": "person",
       "person": {
-        "email": "ceci.kurdelak@solutional.com"
+        "email": "dresdan.tabith@example-domain.com"
       }
     },
     {
       "object": "user",
       "id": "155d872b-594c-810e-9174-00024470271f",
-      "name": "Katherine",
+      "name": "Kate",
       "avatar_url": null,
       "type": "person",
       "person": {
-        "email": "katherine@dirksen.com"
+        "email": "kate@bond.com"
       }
     },
     {
       "object": "user",
       "id": "158d872b-594c-81b0-a53d-0002632ed5fc",
-      "name": "Nick McKibbon",
+      "name": "Nick Furry",
       "avatar_url": null,
       "type": "person",
       "person": {
-        "email": "nick.mckibbon@gmail.com"
+        "email": "nick.furry@gmail.com"
       }
     },
     {
       "object": "user",
       "id": "1bbd872b-594c-8153-b8b4-0002a90b5da6",
-      "name": "Brooklyn",
-      "avatar_url": "https://s3-us-west-2.amazonaws.com/public.notion-static.com/c4741628-7c3f-4c49-b77c-f8301bfe8eb6/BRobinson_headshot_2022.jpeg",
+      "name": "Mississipi",
+      "avatar_url": "placeholder-avatar_url",
       "type": "person",
       "person": {
-        "email": "brooklyn@solutional.com"
+        "email": "mississippi@example-domain.com"
       }
     },
     {
       "object": "user",
       "id": "1d1d872b-594c-819f-bce3-00027369acef",
-      "name": "Phil Gervasi",
+      "name": "Philemon Bocherini",
       "avatar_url": null,
       "type": "person",
       "person": {
-        "email": "phil.gervasi@solutional.com"
+        "email": "philemon.bocherini@example-domain.com"
       }
     },
     {
       "object": "user",
       "id": "1e0d872b-594c-81e5-be86-000260493812",
-      "name": "Harvey Kisiangani",
-      "avatar_url": "https://lh3.googleusercontent.com/a/ACg8ocJPEZrdkCpP57gr8TnO-ONwkZl3ZU4gd3KISv81Esya6wGna9Q=s100",
+      "name": "Harvey Spectre",
+      "avatar_url": "placeholder-avatar-url",
       "type": "person",
       "person": {
-        "email": "harveykisiangani@gmail.com"
+        "email": "harveyspectre@gmail.com"
       }
     },
     {
       "object": "user",
       "id": "22dd872b-594c-81f8-8099-0002f6600181",
-      "name": "Emily Hilbert",
+      "name": "Lillian Liberty",
       "avatar_url": null,
       "type": "person",
       "person": {
-        "email": "emily.hilbert@solutional.com"
+        "email": "lillian.Liberty@solutional.com"
       }
     },
     {
       "object": "user",
       "id": "25cd872b-594c-8111-84e3-00020e1da9ea",
       "name": "Harvey Kisiangani",
-      "avatar_url": "https://s3-us-west-2.amazonaws.com/public.notion-static.com/7482e92f-22ed-4493-a9d4-47676e140688/img-20260103-wa0013.jpg",
+      "avatar_url": "placeholder-avatar-url",
       "type": "person",
       "person": {
-        "email": "harvey.kisiangani@solutional.com"
+        "email": "harvey.spectre@example-domain.com"
       }
     },
     {
       "object": "user",
       "id": "262d872b-594c-816d-9e5e-00029d62603f",
-      "name": "Suzanne Ryerson",
-      "avatar_url": "https://lh3.googleusercontent.com/a/ACg8ocKnTUsqLdHuCBUA-gGEGgOXwj7acp-XTYzGUAez017txcIxWA=s100",
+      "name": "Sue Rhoda",
+      "avatar_url": "placeholder-avatar-url",
       "type": "person",
       "person": {
-        "email": "suzanne@solutional.com"
+        "email": "sue@example-domain.com"
       }
     },
     {
       "object": "user",
       "id": "264d872b-594c-811e-afe6-0002754d4767",
-      "name": "Julia Gervasi",
-      "avatar_url": "https://lh3.googleusercontent.com/a/ACg8ocJMeX8d1QhoyEG62WuOYhRRuytYtcn_v959HQ3hVSWQyvcxHA=s100",
+      "name": "Joana Bocherini",
+      "avatar_url": "placeholder-avatar-url",
       "type": "person",
       "person": {
-        "email": "julia.gervasi@solutional.com"
+        "email": "joana.bocherini@example-domain.com"
       }
     },
     {
       "object": "user",
       "id": "264d872b-594c-814c-add6-00025b9a6dbe",
-      "name": "diana@solutional.com",
+      "name": "amazon@sample-domain.com",
       "avatar_url": null,
       "type": "person",
       "person": {
-        "email": "diana@solutional.com"
+        "email": "amazon@sample-domain.com"
       }
     },
     {
       "object": "user",
       "id": "264d872b-594c-814f-be30-0002491914a1",
-      "name": "hayden.hoy@solutional.com",
+      "name": "hayden.seek@example-domain.com",
       "avatar_url": null,
       "type": "person",
       "person": {
-        "email": "hayden.hoy@solutional.com"
+        "email": "hayden.seek@example-domain.com"
       }
     },
     {
       "object": "user",
       "id": "264d872b-594c-8165-a127-000223d1869e",
-      "name": "John Medina",
+      "name": "John Mendelsohn",
       "avatar_url": null,
       "type": "person",
       "person": {
-        "email": "john.medina@solutional.com"
+        "email": "john.mendelsohn@example-domain.com"
       }
     },
     {
       "object": "user",
       "id": "264d872b-594c-817a-a2b1-00022e7966cb",
-      "name": "utility@solutional.com",
+      "name": "utility@example-domain.com",
       "avatar_url": null,
       "type": "person",
       "person": {
-        "email": "utility@solutional.com"
+        "email": "utility@example-domain.com"
       }
     },
     {
       "object": "user",
       "id": "264d872b-594c-8181-9dcc-0002a1130067",
-      "name": "nikki@solutional.com",
+      "name": "nikkita@domain.com",
       "avatar_url": null,
       "type": "person",
       "person": {
-        "email": "nikki@solutional.com"
+        "email": "nikkita@domain.com"
       }
     },
     {
       "object": "user",
       "id": "264d872b-594c-819c-bb03-0002725087c7",
-      "name": "Diana",
+      "name": "prudence",
       "avatar_url": null,
       "type": "person",
       "person": {
-        "email": "diana.razonable@solutional.com"
+        "email": "diana.prudence@solutional.com"
       }
     },
     {
       "object": "user",
       "id": "264d872b-594c-81bd-9ea8-0002b5ca7b01",
-      "name": "ian.biana@solutional.com",
+      "name": "ian.duncan@my-domain.com",
       "avatar_url": null,
       "type": "person",
       "person": {
-        "email": "ian.biana@solutional.com"
+        "email": "ian.duncan@my-domain.com"
       }
     },
     {
       "object": "user",
       "id": "264d872b-594c-81f4-9623-0002780160a1",
-      "name": "jo.deguzman@solutional.com",
+      "name": "joe.not-average@may-domain.com",
       "avatar_url": null,
       "type": "person",
       "person": {
-        "email": "jo.deguzman@solutional.com"
+        "email": "joe.not-average@my-domain.com"
       }
     },
     {
       "object": "user",
       "id": "264d872b-594c-81f6-92be-000247426fce",
-      "name": "zak.garland@solutional.com",
+      "name": "zak.green@my-domain.com",
       "avatar_url": null,
       "type": "person",
       "person": {
-        "email": "zak.garland@solutional.com"
+        "email": "zak.green@my-domain.com"
       }
     },
     {
       "object": "user",
       "id": "a0aeb683-ecc4-4e9c-8dd2-136239554894",
-      "name": "Keegan Dunn",
-      "avatar_url": "https://lh3.googleusercontent.com/a/AAcHTtdPnd58uvsM1WgmYGLJBv9vyLEW193AR61wIVNgRUG2JQ=s100",
+      "name": "Kevin Davidson",
+      "avatar_url": "placeholder-avatar-url",
       "type": "person",
       "person": {
-        "email": "kmdcmd2005@gmail.com"
+        "email": "kkomodo2005@gmail.com"
       }
     },
     {
       "object": "user",
       "id": "ad835b36-8b72-47ab-8db0-b67f8b77c6d1",
-      "name": "Stephen Jones",
+      "name": "Jeremy Jones",
       "avatar_url": null,
       "type": "person",
       "person": {
-        "email": "stephenchristopherjones@gmail.com"
+        "email": "Jeremyjones@gmail.com"
       }
     },
     {
@@ -230,37 +230,37 @@ export const EXAMPLE_RAW_USERS_RESPONSE: ListUsersResponse = {
       "avatar_url": null,
       "type": "person",
       "person": {
-        "email": "phillipgervasi@gmail.com"
+        "email": "phillemonbocherini@gmail.com"
       }
     },
     {
       "object": "user",
       "id": "136d872b-594c-817b-adaa-00026796be69",
-      "name": "James Dirksen",
+      "name": "James Bond",
       "avatar_url": null,
       "type": "person",
       "person": {
-        "email": "james.dirksen@solutional.com"
+        "email": "james.bond@my-domain.com"
       }
     },
     {
       "object": "user",
       "id": "13dd872b-594c-810f-8bb4-000282e27820",
-      "name": "Daniel Dirksen",
-      "avatar_url": "https://s3-us-west-2.amazonaws.com/public.notion-static.com/bc9f5c02-88ed-4668-971b-c8480fb70fca/DSC_3230.jpg",
+      "name": "Daniel Bond",
+      "avatar_url": "placeholder-avatar-url",
       "type": "person",
       "person": {
-        "email": "daniel.dirksen@solutional.com"
+        "email": "daniel.bond@my-domain.com"
       }
     },
     {
       "object": "user",
       "id": "145d872b-594c-81bb-960d-0002af602358",
-      "name": "Scott Robohn",
-      "avatar_url": "https://s3-us-west-2.amazonaws.com/public.notion-static.com/93c0fe81-eeac-48b6-840d-3faef12414d4/sfr-Jan2025-MINUG.jpeg",
+      "name": "Scott Rhymes",
+      "avatar_url": "",
       "type": "person",
       "person": {
-        "email": "scott.robohn@solutional.com"
+        "email": "scott.rhymees@my-domain.com"
       }
     },
     {
@@ -298,7 +298,7 @@ export const EXAMPLE_RAW_USERS_RESPONSE: ListUsersResponse = {
           "type": "workspace",
           "workspace": true
         },
-        "workspace_name": "Solutional",
+        "workspace_name": "example-domain",
         "workspace_limits": {
           "max_file_upload_size_in_bytes": 5368709120
         }

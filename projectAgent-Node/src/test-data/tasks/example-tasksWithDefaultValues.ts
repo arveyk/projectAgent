604dd7aa0a69348b7aa1c73155c7327eebb7dab1
Ceci Kurdelak <crkurdelak@gmail.com>
2026-03-05 15:36:14 -0800

    Wrote unit tests for setDefaults

---
+++ projectAgent-Node/src/test-data/tasks/example-tasksWithDefaultValues.ts
@@ -0,0 +1,58 @@
+import { DateTime } from "luxon";
+import { UserData } from "../../utils/controllers/getUsersSlack";
+import { Task } from "../../utils/taskFormatting/task";
+
+export const EXAMPLE_USER_DATA_DANA: UserData = {
+    eventTimeData: DateTime.fromISO("2026-03-04"),
+    userId: "12345",
+    name: "Dana Cardinal",
+    email: "dana@nightvale.net"
+}
+
+export const EXAMPLE_TASK_WITH_ALL_REQUIRED_FIELDS: Task = {
+    taskTitle: "Pet the cats",
+    description: "Pet the cats",
+    assignees: [
+        {
+            name: "Dana Cardinal",
+        }
+    ],
+    startDate: "2026-03-04",
+    dueDate: "2026-03-06"
+}
+
+export const EXAMPLE_TASK_MISSING_START_DATE: Task = {
+    taskTitle: "Pet the cats",
+    description: "Pet the cats",
+    assignees: [
+        {
+            name: "Dana Cardinal",
+        }
+    ],
+    dueDate: "2026-03-06"
+}
+
+export const EXAMPLE_TASK_MISSING_DUE_DATE: Task = {
+    taskTitle: "Pet the cats",
+    description: "Pet the cats",
+    assignees: [
+        {
+            name: "Dana Cardinal",
+        }
+    ],
+    startDate: "2026-03-04",
+}
+
+export const EXAMPLE_TASK_MISSING_ASSIGNEES: Task = {
+    taskTitle: "Pet the cats",
+    description: "Pet the cats",
+    assignees: [],
+    startDate: "2026-03-04",
+    dueDate: "2026-03-06"
+}
+
+export const EXAMPLE_TASK_MISSING_START_DUE_ASSIGNEES: Task = {
+    taskTitle: "Pet the cats",
+    description: "Pet the cats",
+    assignees: [],
+}

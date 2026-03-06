9455d846d2c0ba01329cf89d96e3b123a81aa497
Ceci Kurdelak <crkurdelak@gmail.com>
2026-03-06 11:32:24 -0800

    Tested CloudWatch alarm

--- testProjectAgent/index.js
+++ testProjectAgent/index.js
@@ -1,59 +1,14 @@
-/* Post a message to a channel your app is in using ID and message tex
- Require the Node Slack SDK package (github.com/slackapi/node-slack-sdk)
- */
+import axios from "axios";
+import dotenv from "dotenv";
 
-require('dotenv').config()
-const Slack = require('@slack/bolt');
-const { WebClient, LogLevel } = require("@slack/web-api");
-const token = process.env.SLACK_BOT_TOKEN;
-const signingSecret = process.env.SLACK_SIGNING_SECRET;
-const nReadlines = require('n-readlines');
+// Test the CloudWatch alarms by overloading the function
+dotenv.config();
+const REFRESHCACHE_FUNCTION_URL = process.env.REFRESHCACHE_FUNCTION_URL;
 
-const sampleChats = new nReadlines('../samplechats.txt')
-const channelID = process.env.CHANNEL_ID;
-
-const app = new Slack.App({
-  signingSecret: signingSecret,
-  token: token
-});
-// WebClient instantiates a client that can call API methods
-// When using Bolt, you can use either `app.client` or the `client` passed to listeners.
-
-
-const client = new WebClient(token, {
-  // LogLevel can be imported and used to make debugging simpler
-  logLevel: LogLevel.DEBUG
-});
-async function publishMessage(id, text) {
-  try {
-    // Call the chat.postMessage method using the built-in WebClient
-    
-    const result = await app.client.chat.postMessage({
-      // The token you used to initialize your app
-      token: token,
-      channel: id,
-      text: text
-      // You could also use a blocks[] array to send richer content
-    });
-
-
-    // Print result, which includes information about the message (like TS)
-    console.log(result);
-  }
-  catch (error) {
-    console.error(error);
-  }
+const responses = [];
+for (let i = 0; i < 100; i++) {
+  const res = axios.get(REFRESHCACHE_FUNCTION_URL);
+  console.log(res.status);
+  responses.push(res);
 }
-
-let line;
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-(async () => {
-  while (line = sampleChats.next()) {
-    await publishMessage(channelID, line.toString('ascii'));
-    await sleep(120000);
-  }
-})();
-
+//console.log(JSON.stringify(responses.map(res => res.status)));

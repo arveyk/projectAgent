72abc04c14179521d5369b4e812478279ca94887
Ceci Kurdelak <crkurdelak@gmail.com>
2026-03-13 13:18:22 -0700

    Unit tests for langchainAnthropicHealthCheck

--- projectAgent-Node/src/utils/healthCheck/healthCheck.test.ts
+++ projectAgent-Node/src/utils/healthCheck/healthCheck.test.ts
@@ -1,6 +1,10 @@
 import { EXAMPLE_RAW_USERS_RESPONSE } from "../../test-data/cache/rawUsers";
-import { notionHealthCheck, slackHealthCheck, langchainAnthropicHealthCheck } from "./healthCheck";
+import { notionHealthCheck, langchainAnthropicHealthCheck } from "./healthCheck";
 import { Client } from "@notionhq/client";
+import { ChatAnthropic } from "@langchain/anthropic";
+import { AIMessageChunk } from "langchain";
+
+// TODO figure out how to test slackHealthCheck
 
 jest.mock("@notionhq/client", () => {
   return {
@@ -11,6 +15,14 @@ jest.mock("@notionhq/client", () => {
     },
   };
 });
+
+jest.mock("@langchain/anthropic", () => {
+    return {
+        ChatAnthropic: class {
+            invoke = jest.fn()
+        }
+    }
+})
 
 describe("Tests notionHealthCheck", () => {
     beforeEach(() => {
@@ -27,6 +39,30 @@ describe("Tests notionHealthCheck", () => {
         const fakeNotion = new Client();
         jest.spyOn(fakeNotion.users, 'list').mockImplementationOnce(() => {throw new Error("Error for testing")});
         const resp = await notionHealthCheck(fakeNotion);
+        expect(resp.ok).toBeFalsy();
+    })
+    afterEach(() => {
+        jest.clearAllMocks();    // Clears call history
+        jest.resetAllMocks();    // Clears calls + implementation
+        jest.restoreAllMocks();  // Fully restores original behavior
+    });
+})
+
+describe("Tests langchainAnthropicHealthCheck", () => {
+    beforeEach(() => {
+        jest.resetModules();
+        jest.clearAllMocks();
+    })
+    it("Should return ok: true", async () => {
+        const fakeAnthropic = new ChatAnthropic();
+        jest.spyOn(fakeAnthropic, 'invoke').mockResolvedValueOnce(new AIMessageChunk("hello"));
+        const resp = await langchainAnthropicHealthCheck(fakeAnthropic);
+        expect(resp.ok).toBeTruthy();
+    })
+    it("Should return ok: false and an error", async () => {
+        const fakeAnthropic = new ChatAnthropic();
+        jest.spyOn(fakeAnthropic, 'invoke').mockImplementationOnce(() => {throw new Error("Error for testing")});
+        const resp = await langchainAnthropicHealthCheck(fakeAnthropic);
         expect(resp.ok).toBeFalsy();
     })
     afterEach(() => {
